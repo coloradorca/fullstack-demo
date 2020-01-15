cca7b5db864d9a979bfe0d16cf7f156aaac4a8e5
Rob Gonzalez-Pita <robgonzalezpita@gmail.com>
2020-01-15 11:50:59 -0700

    removed esLint, completed backend

--- server/db/queries.js
+++ server/db/queries.js
@@ -1,1 +1,33 @@
-// create db
+const express = require('express');
+var app = require('../index.js')
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/bugs', {useNewUrlParser: true, useUnifiedTopology: true });
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected to mongoose')
+});
+
+
+var Schema = mongoose.Schema;
+
+var bugSchema = new Schema ({
+  bugName: Number,
+  bugDescription: String,
+  reportedBy: String,
+  createdDate: Date,
+  assignedTo: String,
+  threatLevel: String,
+});
+
+var NewBug = mongoose.model('NewBug', bugSchema, 'bugs');
+
+
+module.exports = {
+  NewBug
+}
+
+
--- server/index.js
+++ server/index.js
@@ -1,1 +1,31 @@
 const express = require('express');
+const NewBug = require('./db/queries.js').NewBug;
+const app = express();
+
+const bodyparser = require('body-parser');
+
+app.use(bodyparser.json());
+
+app.post('/bug', (req,res,next) => {
+  console.log(req.body)
+  var newBug = new NewBug(req.body)
+  newBug.save()
+  // .then((results) => res.send(results))
+  res.send("You're bug is being processed")
+})
+
+app.get('/bug', (req, res, next) => {
+  NewBug.find()
+  .then((results) => res.send(results))
+})
+
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+}  )
+
+
+module.exports = {
+  app
+}
